0dc4e25479418e33564c8f4ebc692648eb7a3f94
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-02-04 17:34:02 +0800

    try to implement remove block

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { CONNREFUSED } from 'dns';
 import {Plugin,MarkdownView, TFile} from 'obsidian';
 import { start } from 'repl';
 import YAML from 'yaml'
@@ -100,42 +101,94 @@ export default class MyPlugin extends Plugin {
 		}
 	}
 
+	removeBlock(co:CodeMirror.EditorChangeLinkedList,startLineOfCurrentBlock:number,lines:string[],yamlObject:any,currentFile:TFile,cm:CodeMirror.Editor){
+		console.log("block removed")
+		var removed = co.removed.filter(c=>{
+			if(c.startsWith("^")){
+				return c
+			}
+		})
+		var id:string
+		if(removed){
+			id = removed[0]
+		}else if(startLineOfCurrentBlock == co.from.line-1){
+			id = lines[startLineOfCurrentBlock]
+		}else{
+			return;
+		}
+		if(yamlObject){
+			if(yamlObject.blockTimestamp){
+				yamlObject.blockTimestamp = yamlObject.blockTimestamp.filter((b:any)=>{
+					if(b.id != id)
+						return b;
+				})
+				//update the new content to  not include the current block id
+				var newContent=""
+				for(var n = 0;n<lines.length;n++){
+					if(n != startLineOfCurrentBlock){
+						newContent+=lines[n]+"\n";	
+					}
+				}
+				//update the metadata
+				var yamlString = YAML.stringify(yamlObject)
+				newContent = newContent.replace(/(?<=---\n)(.*)(?=---)/s,yamlString)
+				const cursorPos = cm.getCursor()
+				this.app.vault.modify(currentFile,newContent).then(()=>{
+					var removedContentLength = 4//1 for the new block id in the content, 3 for metadata timestamp in the yaml 
+					cursorPos.line -=(removedContentLength)
+					if(cursorPos.line<0){
+						cursorPos.line = 0
+					} 
+					cm.setCursor(cursorPos)
+				})
+			}
+		}
+	}
+
 	//this seems to be where the plugin started
 	async onload() {
 		console.log('loading plugin');
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			cm.setOption("pollInterval",5000)
 			cm.on("change",(cm,co)=>{
+				console.log(co)
 				const changeTime = this.formatDate(new Date())
 				const startLineOfCurrentBlock = this.getStartLineOfCurrentBlock(cm);
 				const data = cm.getValue()
 				const lines = data.split("\n")
+				//get yaml as object
+				const leaf = this.app.workspace.activeLeaf;
+				if(!leaf)
+					return;
+				const currentView = leaf.view as MarkdownView;
+				const currentFile = currentView.file
+				var metadata = data.match(/(---)(.*)(---)/s);
+				var yamlString = ""
+				if(metadata){
+					yamlString = metadata[0].match(/(?<=---\n)(.*)(?=\n---)/s)[0];
+				}
+				var yamlObject = YAML.parse(yamlString)
 
 				if(startLineOfCurrentBlock == 0 && lines[startLineOfCurrentBlock].startsWith("---")){
 					//cursor is in the metadata
 					return;
 				}
 
-				if(lines[startLineOfCurrentBlock]){
-					//get yaml as object
-					const leaf = this.app.workspace.activeLeaf;
-					if(!leaf)
-						return;
-					const currentView = leaf.view as MarkdownView;
-					const currentFile = currentView.file
-					var metadata = data.match(/(---)(.*)(---)/s);
-					var yamlString = ""
-					if(metadata){
-						yamlString = metadata[0].match(/(?<=---\n)(.*)(?=\n---)/s)[0];
-					}
-					var yamlObject = YAML.parse(yamlString)
+				//user removed a block
+				// if(co.from.line == startLineOfCurrentBlock+1 && lines[co.from.line] == ''){
+				// 	this.removeBlock(co,startLineOfCurrentBlock,lines,yamlObject,currentFile,cm)
+				// }
+				if(lines[co.from.line] == ''){
+					this.removeBlock(co,startLineOfCurrentBlock,lines,yamlObject,currentFile,cm)
+				}
 
+				//user update or create new block
+				if(lines[startLineOfCurrentBlock]){
 					if(!lines[startLineOfCurrentBlock].startsWith("^")){
 						if(this.globalTimeOut !=null) clearTimeout(this.globalTimeOut)
-						this.globalTimeOut = setTimeout(()=>this.createNewBlock(yamlObject,changeTime,lines,startLineOfCurrentBlock,cm,currentFile),200)
+						this.globalTimeOut = setTimeout(()=>this.createNewBlock(yamlObject,changeTime,lines,startLineOfCurrentBlock,cm,currentFile),500)
 					}else{
 						if(this.globalTimeOut !=null) clearTimeout(this.globalTimeOut)
-						this.globalTimeOut = setTimeout(()=>this.updateBlock(yamlObject,changeTime,startLineOfCurrentBlock,lines,currentFile),200)
+						this.globalTimeOut = setTimeout(()=>this.updateBlock(yamlObject,changeTime,startLineOfCurrentBlock,lines,currentFile),500)
 					}
 				}
 			})
