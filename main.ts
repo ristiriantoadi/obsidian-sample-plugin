2a04fa0ea7a30a5744b5844c40d0e432ea78ba88
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-02-04 11:11:09 +0800

    split the file metadata, yaml, and content

--- main.ts
+++ main.ts
@@ -39,22 +39,40 @@ export default class MyPlugin extends Plugin {
 	//this seems to be where the plugin started
 	async onload() {
 		console.log('loading plugin');
-		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Encryption', () => {
-			new EncryptionModal(this.app).open()
+		this.addCommand({
+			id: 'read-current-file',
+			name: 'Read current file',
+			// callback: () => {
+			// 	console.log('Simple Callback');
+			// },
+			checkCallback: (checking: boolean) => {
+				let leaf = this.app.workspace.activeLeaf;
+				if (leaf) {
+					if (!checking) {
+						const currentView = leaf.view as MarkdownView;
+        				const currentFile = currentView.file
+						this.app.vault.read(currentFile).then(data=>{
+							// console.log(data.match(/(?<=---\n)(.*)(?=---)/s)[0].toString())//metadata
+							var metadata = data.match(/(---)(.*)(---)/s)[0].toString();
+							var yaml = metadata.match(/(?<=---\n)(.*)(?=---)/s)[0].toString();
+							var content  = data.split(metadata)[1].toString()
+							console.log(yaml)
+							console.log(content)
+						})
+						// const fileCache = this.app.metadataCache.getFileCache(currentFile)
+						// const frontMatterCache = fileCache.frontmatter
+						// console.log("frontMatterCache",frontMatterCache)
+					}
+					return true;
+				}
+				return false;
+			}
 		});
-
-		this.addRibbonIcon('dice', 'Decryption', () => {
-			new DecryptionModal(this.app).open()
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
 			cm.on("change",(cm,co)=>{
+				return;
 				const changeTime = this.formatDate(new Date())
 				const startLineOfCurrentBlock = this.getStartLineOfCurrentBlock(cm);
 				const lines = cm.getValue().split("\n")
@@ -87,152 +105,9 @@ export default class MyPlugin extends Plugin {
 				}
 			})
 		})
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			const view = this.app.workspace.activeLeaf.view as MarkdownView
-		});
-		
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-
-class EncryptionModal extends Modal{
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const currentView = this.app.workspace.activeLeaf.view as MarkdownView;
-		const currentFile = currentView.file
-		const filename=currentFile.name
-		let {modalEl,titleEl,contentEl} = this;
-		titleEl.setText(`Encrypt: ${filename}`);
-		contentEl.addClass("encryption")
-		var inputKey = new TextComponent(contentEl).setPlaceholder("Enter key ... ")
-		inputKey.inputEl.type="password"
-		const confirmKey = new TextComponent(contentEl).setPlaceholder("Confirm key ... ")
-		confirmKey.inputEl.type="password"
-		new ButtonComponent(contentEl).setButtonText("Encrypt").onClick(()=>{
-			if(inputKey.getValue().length <8){
-				new Notice('8 characters minimum!')
-				return
-			}
-			if(inputKey.getValue() !== confirmKey.getValue()){
-				new Notice('confirmation key wrong!');
-				return;
-			}
-			this.app.vault.read(currentFile).then(text=>{
-				 const key = inputKey.getValue()
-				 const encrypted = aes.encrypt(text, key);
-				 return this.app.vault.modify(currentFile,encrypted.toString())
-			})
-			.then(()=>{
-				this.close()
-			})
-		})
-		console.log(modalEl)
-
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-
-}
-
-class DecryptionModal extends Modal{
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const currentView = this.app.workspace.activeLeaf.view as MarkdownView;
-		const currentFile = currentView.file
-		const filename=currentFile.name
-		let {modalEl,titleEl,contentEl} = this;
-		titleEl.setText(`Decrypt: ${filename}`);
-		contentEl.addClass("encryption")
-		var inputKey = new TextComponent(contentEl).setPlaceholder("Enter key ... ")
-		inputKey.inputEl.type="password"
-		new ButtonComponent(contentEl).setButtonText("Decrypt").onClick(()=>{
-			this.app.vault.read(currentFile).then(text=>{
-				var decrypted = aes.decrypt(text, inputKey.getValue()).toString(CryptoJS.enc.Utf8);
-				return this.app.vault.modify(currentFile,decrypted)
-			})
-			.then(()=>{
-				this.close()
-			})
-			.catch(err=>{
-				new Notice("Wrong key")
-				console.log(err)
-			})
-		})
-		// console.log(modalEl)
-
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Hello WOrld!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
